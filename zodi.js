72562e06541492030533a8780b65bd339b322460
Britk2 <55754895+Britk2@users.noreply.github.com>
2020-01-28 19:46:54 -0500

    Calender/Date Input
    added a date input

--- zodi.js
+++ zodi.js
@@ -1,11 +1,12 @@
 function off() {
     document.getElementById('welcome').hidden = true;
     document.getElementById('ex_job').removeAttribute('hidden');
-
+    document.getElementById('calender').removeAttribute('hidden');
     console.log('Help screen closed');
 }
 function on() {
     document.getElementById('welcome').removeAttribute('hidden');
     document.getElementById('ex_job').hidden = true;
+    document.getElementById('calender').hidden = true;
     console.log('Help screen opened');
 }
