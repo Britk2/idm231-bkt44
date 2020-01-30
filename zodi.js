0e524e80cf74db0cde0e2d50b1c4010689487cca
Britk2 <55754895+Britk2@users.noreply.github.com>
2020-01-30 16:17:16 -0500

    Design clean up & Added title/descrip template

--- zodi.js
+++ zodi.js
@@ -1,12 +1,20 @@
 function wellOff() {
     document.getElementById('welcome').hidden = true;
-    document.getElementById('ex_job').removeAttribute('hidden');
+    document.getElementById('ex_box').removeAttribute('hidden');
     document.getElementById('calender').removeAttribute('hidden');
     console.log('Help screen closed');
 }
 function helpOn() {
     document.getElementById('welcome').removeAttribute('hidden');
-    document.getElementById('ex_job').hidden = true;
+    document.getElementById('ex_box').hidden = true;
     document.getElementById('calender').hidden = true;
+    document.getElementById('des').hidden = true;
+    document.getElementById('info').hidden=true;
     console.log('Help screen opened');
+}
+
+function infoOn() {
+    document.getElementById('info').removeAttribute('hidden');
+    document.getElementById('des').removeAttribute('hidden');
+    console.log('Info Revealed');
 }
