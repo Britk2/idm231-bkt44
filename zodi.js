63ab720b7b7a0320f96e3ba4630ad7d386ef79d9
Britk2 <55754895+Britk2@users.noreply.github.com>
2020-01-29 19:35:15 -0500

    Normalize

--- zodi.js
+++ zodi.js
@@ -1,10 +1,10 @@
-function off() {
+function wellOff() {
     document.getElementById('welcome').hidden = true;
     document.getElementById('ex_job').removeAttribute('hidden');
     document.getElementById('calender').removeAttribute('hidden');
     console.log('Help screen closed');
 }
-function on() {
+function helpOn() {
     document.getElementById('welcome').removeAttribute('hidden');
     document.getElementById('ex_job').hidden = true;
     document.getElementById('calender').hidden = true;
